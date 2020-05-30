648e9435b63d6e7c4613c37058e9789371013204
Alejandro Anzola Avila <anzola95avila@gmail.com>
2020-05-30 10:26:44 -0500

    fixes script for starting backend

--- back/start.sh
+++ back/start.sh
@@ -4,12 +4,12 @@ if ! [ -d '../GlobHack' ]; then
     echo "Creating Python env since it did not exist"
     cd ..
     python3 -m venv GlobHack
-    source GlobHack/bin/activate
+    . GlobHack/bin/activate
     python3 -m pip install -U pip
     cd -
     python3 -m pip install -r requirements.txt
 fi
 
-source ../GlobHack/bin/activate
+. ../GlobHack/bin/activate
 
 python3 manage.py runserver 0:8080
