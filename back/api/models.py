e3474438958cd0b18058d4bd0d539b5656f36e31
Fabian Ardila <fabianra97@gmail.com>
2020-05-30 18:50:21 -0500

    Modify models

--- back/api/models.py
+++ back/api/models.py
@@ -5,6 +5,9 @@ class Sector(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
+    def __str__(self):
+        return "{id: "+ str(self.id) + " || name: "+ str(self.name) + " || description: "+ str(self.description) +"}"
+
 class Company(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -13,8 +16,12 @@ class Company(models.Model):
     suscription = models.BooleanField(default=False)
     sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return "{id: "+ str(self.id)+" || name: " + str(self.name) + " || suscription: " + str(self.suscription) + " || sector: "+ str(self.sector)+"}"
+
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
