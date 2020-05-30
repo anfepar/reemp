4a3ecc3d25f6e8dff931894b142ea88fa6011f86
Fabian Ardila <fabianra97@gmail.com>
2020-05-30 14:09:06 -0500

    Create endpoint realted to Company

--- back/api/views.py
+++ back/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import CompanySerializer
+from .models import Company
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
--- back/back/settings.py
+++ back/back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5qu-^ees-ck-8i!hu%nkqb%2h_1estgjoy0ks%r#&lq+!fo24w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['ec2-18-218-69-56.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS=['ec2-18-218-69-56.us-east-2.compute.amazonaws.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
