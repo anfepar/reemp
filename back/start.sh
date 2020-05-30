cbb3d03024e1f74c87c07c0118e42cb056cd3780
Alejandro Anzola Avila <anzola95avila@gmail.com>
2020-05-30 15:55:00 -0500

    cleans up DB before each deployment

--- back/start.sh
+++ back/start.sh
@@ -12,6 +12,10 @@ fi
 
 . ../GlobHack/bin/activate
 
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc" -delete
+rm db.sqlite3
+
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0:8080
