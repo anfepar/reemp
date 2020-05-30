fce051b9630f0690c7b4d9b97ef00824d8c92b22
Alejandro Anzola Avila <anzola95avila@gmail.com>
2020-05-30 18:19:05 -0500

    does not delete DB on each deploy

--- back/start.sh
+++ back/start.sh
@@ -12,9 +12,9 @@ fi
 
 . ../GlobHack/bin/activate
 
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc" -delete
-rm db.sqlite3
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+# find . -path "*/migrations/*.pyc" -delete
+# rm db.sqlite3
 
 python3 manage.py makemigrations
 python3 manage.py migrate
