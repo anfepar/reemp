c2a66c1117795d1f992f971581087221c025344d
Alejandro Anzola Avila <anzola95avila@gmail.com>
2020-05-30 10:46:49 -0500

    adds config for aws

--- back/back/settings.py
+++ back/back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5qu-^ees-ck-8i!hu%nkqb%2h_1estgjoy0ks%r#&lq+!fo24w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['EC2_DNS_NAME']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+
