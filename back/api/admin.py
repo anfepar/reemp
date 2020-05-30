1275cda40b0fb20ac68a4efcf65d01e24cc87284
Fabian Ardila <fabianra97@gmail.com>
2020-05-30 11:42:28 -0500

    Create models

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Sector)
+admin.site.register(Company)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Product)
--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Sector(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    suscription = models.BooleanField(default=False)
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+
+class Preference(models.Model):
+    id = models.AutoField(primary_key=True)
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
