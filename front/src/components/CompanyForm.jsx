63d2d335621f9484f9e9cd8afaf034769e835db2
Juan David Ramirez Mendoza <c4pjackie@gmail.com>
2020-05-30 18:58:00 -0500

    Company form consumo de servicios

--- front/src/components/CompanyForm.jsx
+++ front/src/components/CompanyForm.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
-import { FormControl, InputLabel, Input, Button, TextField, Select,MenuItem } from "@material-ui/core";
-import {Link} from "react-router-dom"
+import { TextField, Select, MenuItem } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { GET_METHOD } from "../constants/STRINGS";
+import { GET_METHOD, MOCKED_DATA } from "../constants/STRINGS";
 import { serviceMethod } from "../api/util";
 
 import { setDatosCompany, setLocation } from "../actions";
 class CompanyForm extends Component {
-  state = {    
+  state = {
     isLoading: true,
-    countries: [{name:"country1"},{name:"country2"},{name:"country3"},{name:"country4"}],
-    cities: [{name:"city1"},{name:"city2"},{name:"city3"},{name:"city4"}],
-    sectors:[{name:"sector1"},{name:"sector2"},{name:"sector3"},{name:"sector4"}]
+    countries: MOCKED_DATA.COUNTRIES,
+    cities: MOCKED_DATA.CITIES.COL,
+    sectors: [],
   };
 
   componentDidMount() {
     let callback = {
       onSuccess: (response) => {
-        console.log(response);
+        this.setState({ isLoading: false, sectors: response.data }, () => {
+          console.log(this.state.sectors);
+        });
       },
       onFailed: (error) => {
         console.log(error);
@@ -25,75 +27,96 @@ class CompanyForm extends Component {
     };
     serviceMethod(
       GET_METHOD,
-      "http://dummy.restapiexample.com/api/v1/employees",
+      "http://ec2-18-218-69-56.us-east-2.compute.amazonaws.com:8080/sectors",
       {},
       callback
     );
   }
 
-  onChangeHandler = (event,type) => {  
-    console.log(type,event.target.value);
-    if(event.target.name){
-      if(type === "location"){
-        this.props.setLocation({[event.target.name]: event.target.value});
-      }
-      else{
-        this.props.setDatosCompany({[event.target.name]: event.target.value})
+  onChangeHandler = (event, type) => {
+    console.log(type, event.target.value);
+    if (event.target.name) {
+      if (type === "location") {
+        this.props.setLocation({ [event.target.name]: event.target.value });
+      } else {
+        this.props.setDatosCompany({ [event.target.name]: event.target.value });
       }
     }
-    
   };
-  
 
   render() {
-    return (
+    return this.state.isLoading ? (
+      <h1>Loading...</h1>
+    ) : (
       <div>
-          <h1>1. Tu empresa</h1>
-          <p>Registra los datos de tu emprendimiento</p>
-          <form noValidate autoComplete="off">
-            <TextField name="name" id="name" onChange={(e)=> this.onChangeHandler(e)} label="Nombre empresa"  variant="outlined" />
-            <TextField name="nit" id="NIT" onChange={(e)=> this.onChangeHandler(e)} label="NIT"  variant="outlined"/>
-            <TextField name="owner" id="representanteLegal"  onChange={(e)=> this.onChangeHandler(e)} label="Representante Legal" variant="outlined" />
-            <Select
-              name="country"
-              id="demo-simple-select-filled"
-              onChange={(e)=> this.onChangeHandler(e,"location") }
-            >
-              {this.state.countries.map((country,index)=>
-                <MenuItem key={country.name}  value={country}>{country.name}</MenuItem>
-              )}
-            </Select>
-            <Select
-              name="city"
-              id="demo-simple-select-filled"
-              onChange={(e)=> this.onChangeHandler(e,"location") }
-            >
-              {this.state.cities.map((city,index)=>
-                <MenuItem key={city.name} value={city}>{city.name}</MenuItem>
-              )}
-            </Select>
-            <Select
-              name="sector"
-              id="demo-simple-select-filled"
-              onChange={(e)=> this.onChangeHandler(e)}
-            >
-              {this.state.sectors.map((sector,index)=>
-                <MenuItem key={sector.name}  value={sector}>{sector.name}</MenuItem>
-              )}
-            </Select>
-          </form>
-          <Link to="/preferencias">
-          Siguiente
-          </Link>
+        <h1>1. Tu empresa</h1>
+        <p>Registra los datos de tu emprendimiento</p>
+        <form noValidate autoComplete="off">
+          <TextField
+            name="name"
+            id="name"
+            onChange={(e) => this.onChangeHandler(e)}
+            label="Nombre empresa"
+            variant="outlined"
+          />
+          <TextField
+            name="nit"
+            id="NIT"
+            onChange={(e) => this.onChangeHandler(e)}
+            label="NIT"
+            variant="outlined"
+          />
+          <TextField
+            name="owner"
+            id="representanteLegal"
+            onChange={(e) => this.onChangeHandler(e)}
+            label="Representante Legal"
+            variant="outlined"
+          />
+          <Select
+            name="country"
+            id="demo-simple-select-filled"
+            onChange={(e) => this.onChangeHandler(e, "location")}
+          >
+            {this.state.countries.map((country, index) => (
+              <MenuItem key={country.name} value={country}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <Select
+            name="city"
+            id="demo-simple-select-filled"
+            onChange={(e) => this.onChangeHandler(e, "location")}
+          >
+            {this.state.cities.map((city, index) => (
+              <MenuItem key={city.name} value={city}>
+                {city.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <Select
+            name="sector"
+            id="demo-simple-select-filled"
+            onChange={(e) => this.onChangeHandler(e)}
+          >
+            {this.state.sectors.map((sector, index) => (
+              <MenuItem key={sector.name} value={sector}>
+                {sector.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </form>
+        <Link to="/preferencias">Siguiente</Link>
       </div>
-    )
-   // );
+    );
+    // );
   }
 }
 
 const mapDispatchToProps = {
   setDatosCompany,
-  setLocation
+  setLocation,
 };
 
-export default connect(null,mapDispatchToProps)(CompanyForm);
+export default connect(null, mapDispatchToProps)(CompanyForm);
--- front/src/constants/STRINGS.js
+++ front/src/constants/STRINGS.js
@@ -4,6 +4,40 @@ const STRINGS = {
   GET_METHOD: "GET",
   POST_METHOD: "POST",
   PUT_METHOD: "PUT",
+  MOCKED_DATA: {
+    COUNTRIES: [
+      { name: "Argentina", id: "ARG" },
+      { name: "Bolivia", id: "BOL" },
+      { name: "Brasil", id: "BR" },
+      { name: "Chile", id: "CH" },
+      { name: "Colombia", id: "CO" },
+      { name: "Costa Rica", id: "CR" },
+      { name: "Cuba", id: "CU" },
+      { name: "Ecuador", id: "EC" },
+      { name: "Guatemala", id: "GU" },
+      {
+        name: "Haití",
+        id: "HA",
+      },
+      { name: "México", id: "MX" },
+      { name: "Nicaragua", id: "NI" },
+      { name: "Panamá", id: "PA" },
+      { name: "Perú", id: "PE" },
+      { name: "Uruguay", id: "UR" },
+      { name: "Venezuela", id: "VE" },
+      { name: "República dominicana", id: "RD" },
+    ],
+    CITIES: {
+      COL: [
+        { id: "1", name: "Bogotá" },
+        { id: "2", name: "Medellín" },
+        { id: "3", name: "Cali" },
+        { id: "4", name: "Barranquilla" },
+        { id: "5", name: "Cartagena" },
+        { id: "6", name: "Bucaramanga" },
+      ],
+    },
+  },
 };
 
 module.exports = STRINGS;
