9d5b95d0e1e3314dc9e5d2709d516b8ef5f7fa5d
Alejandro Anzola Avila <anzola95avila@gmail.com>
2020-05-30 13:25:35 -0500

    remove migrations folder

--- back/deploy.sh
+++ back/deploy.sh
@@ -2,7 +2,7 @@
 
 if ! [ -z "$PRODUCTION" ]; then 
     echo "************ Production environment, detaching from terminal ************"
-    nohup sh start.sh &
+    nohup sh start.sh > /dev/null &
 else 
     echo "<<<<<<<<<<<<<< Development server <<<<<<<<<<<<<<"
     sh start.sh
