cec04f1907c022aafd9fff0f01f4ba4f36404eb7
Fabian Ardila <fabianra97@gmail.com>
2020-05-31 10:32:07 -0500

    Add RUD to alliance model

--- back/api/urls.py
+++ back/api/urls.py
@@ -12,5 +12,6 @@ router.register(r'locations', views.LocationViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('alliance/', views.alliance_view),
+    path('alliance/<int:pk>', views.alliance_detail),
     #path('get_best_company_matches/<int:pk>', views.get_best_company_matches)
 ]
--- back/api/views.py
+++ back/api/views.py
@@ -58,6 +58,33 @@ def alliance_view(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+
+@csrf_exempt
+def alliance_detail(request, pk):
+    """
+    Retrieve, update or delete a code alliance.
+    """
+    try:
+        alliance = Alliance.objects.get(pk=pk)
+    except Alliance.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = AllianceSerializer(alliance)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = AllianceSerializer(alliance, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        alliance.delete()
+        return HttpResponse(status=204)
+
 """@api_view(["GET"])
 def get_best_company_matches(request, pk):
     try:
