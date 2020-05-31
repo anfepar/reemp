22bd68d79bbbe634896a6a105906cc3b2c56564a
Juan David Ramirez Mendoza <c4pjackie@gmail.com>
2020-05-31 12:00:56 -0500

    Textfields from material ui

--- front/src/components/ProductsAlliance.jsx
+++ front/src/components/ProductsAlliance.jsx
@@ -4,6 +4,7 @@ import CardProduct from "./CardProduct";
 import { Link } from "react-router-dom";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 import NavigateScreenIcon from "./NavigateScreenIcon";
+import { TextField } from "@material-ui/core";
 
 class ProductsAlliance extends Component {
   state = {
@@ -18,14 +19,46 @@ class ProductsAlliance extends Component {
         name: "PRUEBA",
       },
     ],
+    startDate: "",
+    endDate: "",
+  };
+
+  onchangeHandler = (e) => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      console.log(this.state);
+    });
+    console.log("PRESIONADO");
   };
   render() {
     return (
       <div className="flex flex-row space-between">
         <div className="flex center flex-column">
           <div className="flex space-between flex-row">
-            <h1>DD//MM/AAA</h1>
-            <h1>Dasdadsa</h1>
+            <TextField
+              id="startDate"
+              label="Fecha Inicio"
+              name="startDate"
+              type="date"
+              onChange={this.onchangeHandler}
+              value={this.state.startDate}
+            />
+            <TextField
+              id="endDate"
+              label="Fecha Finalización"
+              name="endDate"
+              type="date"
+              onChange={this.onchangeHandler}
+              value={this.state.endDate}
+            />
+            {/* <div>
+              <label for="startDate">Fecha Inicio</label>
+              <input type="date" id="startDate" name="startDate" />
+            </div>
+            <div>
+              <label for="endDate">Fecha Finalizacion</label>
+              <input type="date" id="endDate" name="endDate" />
+            </div> */}
           </div>
           <div className="flex flex-row space-between">
             {this.state.products.map((product) => {
--- front/src/containers/Register.jsx
+++ front/src/containers/Register.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import UserForm from '../components/UserForm';
-import CompanyForm from '../components/CompanyForm';
-import PreferencesForm from '../components/PreferencesForm';
-import Button from '@material-ui/core/Button'
+import UserForm from "../components/UserForm";
+import CompanyForm from "../components/CompanyForm";
+import PreferencesForm from "../components/PreferencesForm";
+import Button from "@material-ui/core/Button";
 
 export default class Register extends Component {
   constructor(props) {
