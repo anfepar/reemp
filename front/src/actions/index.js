9e6c816d0c720157dec10a99835e82dc68572c6d
Andres Felipe Pardo Mesa <fpardo@muvobike.com>
2020-05-31 11:54:57 -0500

    fix conflict

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -12,15 +12,11 @@ export const loginCompany = (payload) => ({
   type: "LOGIN_COMPANY",
   payload,
 });
-
-<<<<<<< HEAD
 export const selectAlly = (payload) => ({
   type: "SELECT_ALLY",
   payload,
 })
-=======
 export const setTituloAlianza = (payload) => ({
   type: "SET_TITULO_ALIANZA",
   payload,
 });
->>>>>>> c6019d5517a6dcd1b27e8bab05f66730999eac24
